ea1a412eaa9ea971ecc0d565612083e8f47907fd
kjgardner <52870775+kjgardner@users.noreply.github.com>
2021-06-22 02:14:53 +0000

    Part 2 (still need 1.2 b..)

--- candidate-testing.js
+++ candidate-testing.js
@@ -2,14 +2,29 @@ const input = require('readline-sync');
 
 // TODO 2: modify your quiz app to ask 5 questions //
 
+
+
 // TODO 1.1a: Define candidateName // 
 let candidateName = "";
+
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions;
-let correctAnswers;
+let questions = [
+  "Who was the first American woman in space? ",
+  "True of false: 5 kilometer == 500 meters? ",
+  "(5+3)/2*10 = ? ",
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+  "What is the minmum crew size for the ISS? "
+];
+let correctAnswers = [
+  "Sally Ride",
+  "true",
+  "40",
+  "Trajectory",
+  "3"
+];
 let candidateAnswers;
 
 
@@ -20,9 +35,13 @@ candidateName = input.question("Enter your name:");
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question(question);
+ candidateAnswer = input.question(questions);
 
+for (let i = 0; i < questions.length; i++) {
+   console.log(questions[i]);
 }
+}
+
 
 function gradeQuiz(candidateAnswers) {
 
