4a37da638d9ce903fb314e9861c225c567c472b4
kjgardner <52870775+kjgardner@users.noreply.github.com>
2021-06-28 21:24:27 +0000

    attempt a assignment 1 but I'm still trying to figure out how to store user's input (candidateAnswers) into an array

--- candidate-testing.js
+++ candidate-testing.js
@@ -2,21 +2,18 @@ const input = require('readline-sync');
 
 // TODO 2: modify your quiz app to ask 5 questions //
 
-
-
 // TODO 1.1a: Define candidateName // 
-let candidateName = "";
-
+let candidateName = ""
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "Who was the first American woman in space? ";
+let question = "Who was the first American woman in space? "
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
 let questions = [
-  "Who was the first American woman in space? ",
-  "True of false: 5 kilometer == 500 meters? ",
-  "(5+3)/2*10 = ? ",
-  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
-  "What is the minmum crew size for the ISS? "
+  "Who was the first American woman in space?",
+  "True or false: 5 kilometer == 5000 meters?",
+  "(5 + 3)/2 * 10 = ?",
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?",	
+  "What is the minimum crew size for the ISS?"
 ];
 let correctAnswers = [
   "Sally Ride",
@@ -25,43 +22,48 @@ let correctAnswers = [
   "Trajectory",
   "3"
 ];
-let candidateAnswers;
+let candidateAnswers  = [];
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-candidateName = input.question("Enter your name:");
+candidateName = input.question("What is your name:");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
- candidateAnswer = input.question(questions);
 
 for (let i = 0; i < questions.length; i++) {
-   console.log(questions[i]);
-}
+  console.log(candidateAnswers = input.question(questions[i]));
 }
 
+}
 
 function gradeQuiz(candidateAnswers) {
 
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
-if (candidateAnswer === correctAnswer) {
-  console.log("That's correct, way to go!")
-} else {
-  console.log("Wrong, google it baby!")
-}
-  let grade;
-  
+console.log(`Your answers:${candidateAnswers}\nCorrect anwers: ${correctAnswers}`)
+  /* I'M TRYING TO COMPARE CANDIDATE ANSWERS....
+  let total = 0
+ if (correctAnswers[0] === candidateAnswers[0]) {
+   total += 1
+ }
+*/
+  let grade = total / 5 * 100;
 
   return grade;
+ 
+ if (grade >= 80) {
+   console.log (`Congratulations you passed: ${grade}`)
+ } else if (grade < 80) {
+   console.log (`Failed: ${grade}`)
+ }
+
 }
 
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  console.log("Hello, " + candidateName);
+ console.log(`Hello, ${candidateName}`) 
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
