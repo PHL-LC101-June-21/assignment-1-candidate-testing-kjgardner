82fad896c3132ccf0fa3844b55228be27d11ce3c
kjgardner <52870775+kjgardner@users.noreply.github.com>
2021-06-29 04:47:05 +0000

    assignment 1 attempt

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,51 +3,51 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName = ""
+let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "Who was the first American woman in space? "
-let correctAnswer = "Sally Ride";
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride ";
 let candidateAnswer = "";
 let questions = [
-  "Who was the first American woman in space?",
-  "True or false: 5 kilometer == 5000 meters?",
-  "(5 + 3)/2 * 10 = ?",
-  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?",	
-  "What is the minimum crew size for the ISS?"
+  "Who was the first American woman in space? ",
+  "True or false: 5 kilometer == 5000 meters? ",
+  "(5 + 3)/2 * 10 = ? ",
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",	
+  "What is the minimum crew size for the ISS? "
 ];
 let correctAnswers = [
-  "Sally Ride",
-  "true",
-  "40",
-  "Trajectory",
-  "3"
+  " Sally Ride", 
+  " true",
+  " 40",
+  " Trajectory",
+  " 3 "
 ];
-let candidateAnswers  = [];
+let candidateAnswers = [];
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-candidateName = input.question("What is your name:");
+candidateName = input.question("What is your name: ");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
 for (let i = 0; i < questions.length; i++) {
-  console.log(candidateAnswers = input.question(questions[i]));
+  candidateAnswers.push (input.question(questions[i]));
 }
-
 }
-
 function gradeQuiz(candidateAnswers) {
 
 console.log(`Your answers:${candidateAnswers}\nCorrect anwers: ${correctAnswers}`)
-  /* I'M TRYING TO COMPARE CANDIDATE ANSWERS....
+  // I'M TRYING TO COMPARE CANDIDATE ANSWERS....
   let total = 0
- if (correctAnswers[0] === candidateAnswers[0]) {
+  for (let i = 0; i < questions.length; i++)
+ if (correctAnswers[i] === candidateAnswers[i]) {
    total += 1
  }
-*/
+console.log(total)
+
   let grade = total / 5 * 100;
 
   return grade;
